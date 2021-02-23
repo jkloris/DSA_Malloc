badb1cb173caa5ee803a6229d56a1b600db82a5d
jkloris <kapusta.jergus@gmail.com>
2021-02-23 15:06:17 +0100

    init commit

---
+++ Source.c
@@ -0,0 +1,154 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MEMSIZE 40
+
+//typedef struct _head {
+//	short size;
+//	char n;
+//}HEAD;
+
+void** memP;
+void show_memory();
+void memory_init(void* ptr, unsigned int size);
+void* memory_alloc(unsigned int size);
+
+void test();
+int main() {
+
+	char  memory[MEMSIZE];
+	//*p = &memory[10];
+	memory_init(memory, MEMSIZE);
+	
+	void** p;
+	p = memP;
+	test();
+	return 0;
+}
+//void show_memory() {
+//	char *p = (char*)memP;
+//	for (int i = 0; i < MEMSIZE; i++) {
+//		if (*(p + i) == NULL) printf("NULL\n");
+//		else printf("%c\n", *(p + i));
+//	}
+//}
+
+void* memory_alloc(unsigned int size) {
+
+}
+
+void memory_init(void* ptr, unsigned int size) {
+
+	void** p = ptr;
+	*p = (void**)ptr + 1; // pointer to pointer ptr[sizeof(void*)]
+
+	p = p + 1;
+	unsigned int BuffSize = (size << 1); //velkost s bitom na urcenie zaplnenia
+	*(unsigned int*)p = BuffSize;
+
+	p = (char*)ptr + sizeof(char) * (size - sizeof(unsigned int)); //poslednych sizeof(int) miest memory pola
+	*(unsigned int*)p = BuffSize;
+	
+	
+	//next a last ptr su zatial NULL
+	p = (char*)ptr + sizeof(void*) + sizeof(unsigned int) ;
+	*p = NULL;
+	p = p + 1;
+	*p = NULL;
+
+	p = ptr;
+	BuffSize = **(unsigned int**)p;
+
+	
+
+
+
+	memP = ptr;
+	
+
+}
+
+void test() {
+	
+	char pole[20];
+	
+	//void** p;
+	void *ptr;
+	ptr = pole ;
+	*(unsigned int*)ptr = 50001;
+
+	int a =  * (unsigned int*)ptr;
+	a = (a << 1) | 1;
+
+	*(unsigned int*)ptr = a;
+	a = *(unsigned int*)ptr >> 1;
+	
+	printf("%d\n", a);
+	////init
+	//*(short*)ptr = 20;
+	//ptr = (char*)ptr + 20 - sizeof(short);
+	//*(short*)ptr = 20;
+
+	////malloc
+	//short a = 10;
+
+	//ptr = pole;
+	//*(short*)ptr = a;
+	//ptr = (char*)ptr + sizeof(short);
+	//*(short*)ptr = a + sizeof(short)*2 - 1;
+	//ptr = (char*)ptr + (a + sizeof(short) ) ;
+	//*(short*)ptr = a;
+
+	//ptr = pole + sizeof(short);
+	//short b = *(short*)ptr;
+	//ptr = (char*)ptr + (a + sizeof(short)*2);
+	//*(short*)ptr = b;
+
+	//HEAD* ph;
+//	p = pole;
+//	*(unsigned short*)p = 99;
+//	*(unsigned short*)p = *(unsigned short*)p & 61440;
+//	
+//	printf("%hu\n",  (1 << (sizeof(unsigned short)*7) + 1111));
+//
+///*
+//	HEAD h;
+//	h.size = 10;
+//	h.n = 'a';
+//*/
+//	p = pole; //p ukazuje na zaciatok ptr
+//	//*ph = h;//vytvori pointer na miesto kde ukazuje p odkazujuci na adresu pravej strany '='
+//
+//	//*ph->next = pole[10];
+//
+//	//*p = &pole[10];
+//	//ptr = ptr + 5;
+//	**(short**)p= 65000;
+//	*p = &pole[2];
+//	**(char**)p = 'c';
+//	ptr = (void**)ptr + 1;
+//	*p = &ptr;
+//
+//
+//	*(char*)ptr = 'a';
+//	ptr= (char*)ptr + 1;
+//	*(char*)ptr= 'a';
+//	ptr = (char*)ptr + 2;
+//
+//	*(char*)ptr = 'a';
+//
+//	//*p = (char)ptr + 8;
+//	**(char**)p = 'b';
+//
+//	/*char* buff = malloc(sizeof(char) * 100);
+//	p = pole;
+//
+
+	//pole[0] = 'A';
+	//printf("%c %d\n", pole[0], pole[0]);
+	//*(int*)p = 1000;
+	//printf("%c %d\n", *(char*)p,pole[0]);
+	//p = (int*)p + 2;
+	//*(char*)p = 'a';
+	//printf("%c %c\n",*(char*)p, pole[0] );
+
+}
---
+++ notes.md
@@ -0,0 +1,24 @@
+Zistit si:
+	* ci pri akolokovanej pamati pri presahu pristupi do inej pamate - Pravdepodobne nie
+	* free(p+N) prejde a ak hej, co uvolni - NEDA SA
+
+Otazky na cviko:
+	* ~~staci riesit alokovanie a spol? ~~
+	* ~~~pole ma byt char? ANO~~
+	* ~~ma to byt spravene sposobom ako v priklade? - pole v maine sa potom posiela do mem_init - jo~~
+	* ~~mame tam ukladat hodnoty? Ak ano, hocijakeho typu (int, char, float,..) - konvergovanie ~~
+	* ~~ako to je s explicitnym a implicitnym riesenim - na plnku treba 3. riesenie~~
+	* ~~aka je minimalna a max velkost memory pola - nie je~~
+
+Vyriesit:
+	* kontrola ze som za alokovanou pamatou - stasi pre hlavnu pamat
+	* kontrola ci okolo uvolnovaneho pola je volne miesto
+	* ako ulozit pointer na zadanej adrese - DONE
+	* ako riesit pole < 16
+
+Mozno vyuzi:
+	*	//**(char**)p = 204;
+		//printf("%x\n", **(char**)p);
+	
+	*	//void** p = ptr;
+		//*p = (void**)ptr + 1;
